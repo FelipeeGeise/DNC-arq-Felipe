51203299bfb93c189d148c3f0439435928c93c03
FelipeeGeise <felipe19852024@hotmail.com>
2025-02-21 17:56:31 -0300

    Corrigir caminho do componente Header

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,5 @@
 
-import Header from "../components/Header/header"
+import Header from "../components/Header/Header"
 import Hero from "../components/Hero/Hero"
 import Footer from "../components/Footer/Footer"
 import ProjectsList from "../components/ProjectsList/ProjectsList"
